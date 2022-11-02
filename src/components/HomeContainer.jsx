1577b1e00cf35e94f6d9060dfd91188cd8546005
sanduni <cst18045@std.uwu.ac.lk>
2022-11-02 14:54:26 +0530

    chngs

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -36,7 +36,7 @@ const HomeContainer = () => {
 
         <button
           type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          className="bg-gradient-to-br from-blue- to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Order Now
         </button>
