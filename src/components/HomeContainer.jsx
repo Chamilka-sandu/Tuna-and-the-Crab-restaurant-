d50d65966db9e7414153e12b14144f3f66d53618
sanduni <cst18045@std.uwu.ac.lk>
2022-11-06 17:30:25 +0530

    changes

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -11,8 +11,8 @@ const HomeContainer = () => {
     >
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
-          <p className="text-base text-orange-500 font-semibold">
-            Bike Delivery
+          <p className="text-base text-amber-800 font-semibold">
+            Delivery available 
           </p>
           <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img
@@ -31,12 +31,12 @@ const HomeContainer = () => {
         </p>
 
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          The resturant special items for all type . come and join with us. 
+          Situated in the Galle Dutch Hospital within the Galle Fort, The Tuna & The Crab, a Dharshan Munidasa creation, combines the best of Nihonbashi and Ministry of Crab. With indoor seating for 40 people, this hybrid Seafood and Japanese restaurant serves a selection of sushi, seafood and steak dishes.
         </p>
 
         <button
           type="button"
-          className="bg-gradient-to-br from-blue- to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          className="bg-gradient-to-br from to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Order Now
         </button>
